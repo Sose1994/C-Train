04d6452c57e79944a1b713dcb0397a50ce7fc01b
Sose <sose94@gmail.com>
2017-10-12 15:00:26 +0200

    sending 0's and 1's

---
+++ pulse_with_interrupts.ino
@@ -0,0 +1,115 @@
+char pin_stateness = 0;
+char OUTPTN = 5;
+char previousness = 0;
+char whichBit = 0;
+
+void setup() 
+{
+  // put your setup code here, to run once:
+
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+
+}
+
+
+ISR(TIMER2_COMPA_vect)
+{
+	//Write 1's
+	if (pin_stateness == 0)
+	{
+		digitalWrite(OUTPTN, HIGH);
+		pin_stateness = 1;
+	}
+	else  
+	{
+		digitalWrite(OUTPTN, LOW);
+		pin_stateness = 0;
+	}
+
+
+	if (whichBit == 0)
+	{
+		//Write 0's
+		if (previousness == 0 && pin_stateness == 0)
+		{
+
+			digitalWrite(OUTPTN, HIGH);
+			pin_stateness = 1;
+			previousness = 0;
+
+		}
+		else if (previousness == 1 && pin_stateness == 1)
+		{
+			digitalWrite(OUTPTN, LOW);
+			pin_stateness = 0;
+			previousness = 1;
+		}
+
+		else if (previousness == 0 && pin_stateness == 1)
+		{
+			digitalWrite(OUTPTN, HIGH);
+			pin_stateness = 1;
+			previousness = 1;
+		}
+
+		else if (previousness == 1 && pin_stateness == 0;)
+		{
+			digitalWrite(OUTPTN, LOW);
+			pin_stateness = 0;
+			previousness = 0; 
+		}	
+	}
+	else if (whichBit == 1)
+	{
+			if (pin_stateness == 0)
+		{
+			digitalWrite(OUTPTN, HIGH);
+			pin_stateness = 1;
+		}
+		else  
+		{
+			digitalWrite(OUTPTN, LOW);
+			pin_stateness = 0;
+		}
+	}
+}
+
+
+
+void timer2_setup() 
+{
+// Don't get interrupted while setting up the timer
+	noInterrupts();
+	////
+	//// Set timer2 interrupt at 58 microseconds interval
+	////
+	TCCR2A = 0; // set entire TCCR2A register to 0
+	TCCR2B = 0; // same for TCCR2B
+	TCNT2  = 0; // initialize counter value to 0
+
+	// 58 microseconds interval is 17,241 kHz
+	// set compare match register for 17 kHz increments:
+	// register = (Arduino clock speed) / ((desired interrupt frequency) * prescaler) - 1
+	// register = (16*10^6) / (17.241 * 8) - 1 
+	// register = 115
+	// (must be <256 because timer2 is only 8 bit (timer1 is 16 bit))
+	OCR2A = 115; 
+	// = 116 ticks before reset (because 0 counts)
+	// = 58 microseconds for each tick at 2 MHz
+
+	// Turn on CTC mode
+	TCCR2A |= (1 << WGM21);
+
+	// Set CS21 bit for 8 prescaler (and not CS20 or CS22)
+	TCCR2B |= (1 << CS21); // = 2 MHz = 0,5 microseconds/tick
+	// (0 << CS20) || (0 << CS21) == 0
+
+	// Enable timer compare interrupt
+	TIMSK2 |= (1 << OCIE2A);
+
+	interrupts(); // Reenable interrupts
+}
