7eae3ee3af336d0a8e24d58a03d7378dd9fd1bc9
Sose <sose94@gmail.com>
2017-10-26 17:25:05 +0200

    lille fejl med ternary operation

--- pulse_with_interrupts.ino
+++ pulse_with_interrupts.ino
@@ -1,40 +1,90 @@
 //Playing around with interrupts
 
-char OUTPTN = 5;
+char OUTPIN = 5;
 char whichBit = 0;
 char prevHighOrLow = 0;
 char highOrLow = 0;
 char counter = 0;
-char countBit = 0;
 
 unsigned char isRunAgain = false;
-unsigned char bitMask = 128; 
-
-unsigned char address = 0x24;
-unsigned char order = 0x40;
-unsigned char direction = 1;
-unsigned char speed = 13;
-unsigned char checksum  = address ^ order;
-char ordering = 0;
+unsigned char bitMask = 0x80; 
+unsigned char address = 0;
+unsigned char order = 0;
+unsigned char checksum  = 0x0;
 
 char state = 0;
+//-------------------------------------------------------------------------------------------------------------//
+//0x24 = 36, 0x28 = 40
+unsigned char trainAddress = 40;
+unsigned char trainOrder = 0x40;
+unsigned char trainDirection = 0x0;
+unsigned char trainSpeed = 8;
+unsigned char trainChecksum = 0x0;
+
+//-------------------------------------------------------------------------------------------------------------//
+
+unsigned char accessoryNumber = 101;
+unsigned char accessoryAddress = 0;
+unsigned char accessoryDirection = 1;
+unsigned char accessoryOutput = 0;
+unsigned char accessorySwitch = 0;
+unsigned char accessoryOrder = 0;
+unsigned char packetType = 0;
 
 void setup() 
 {
   // put your setup code here, to run once:
-		
-		if (direction == 1)
-		{
-			order |= 0x20;
-		}
-		
-		if (1 & speed == 1)
-		{
-			speed |= 0x20;
-		}
 
-		speed >>= 1;
-		order |= speed;
+	timer2_setup();
+	pinMode(OUTPIN, OUTPUT);
+  Serial.begin(9600);
+
+
+	if (trainDirection == 1)
+	{
+		trainOrder |= 0x20;
+	}
+	
+	if (1 & trainSpeed == 1)
+	{
+		trainSpeed |= 0x20;
+	}
+
+	trainSpeed >>= 1;
+	trainOrder |= trainSpeed;
+
+
+	accessoryAddress = (accessoryNumber/4) + 1;
+	accessoryAddress |= 0x80;
+
+	accessoryOutput = (accessoryNumber % 4) - 1;
+	if (accessoryOutput < 0) 
+	{
+		accessoryOutput = 3;
+	}
+
+	accessoryOrder = accessoryOrder | 0xF8;
+
+	accessoryOrder |= (accessoryOutput << 1);
+
+	accessoryOrder |= accessoryDirection;
+
+
+	if (packetType == 0)
+	{
+		address = trainAddress;
+		order = trainOrder;
+	}
+	else if (packetType == 1)
+	{
+		address = accessoryAddress;
+		order = accessoryOrder;
+	}
+	checksum = address ^ order;
+
+ Serial.println(address);
+ Serial.println(order);
+ Serial.println(checksum);
 }
 
 void loop() 
@@ -43,7 +93,16 @@ void loop()
 
 }
 
-void preamblePacketWithSkille()
+void accessoryStuff()
+{
+	accessoryOrder ^=  0x08;
+	order = accessoryOrder;
+
+	checksum = address ^ order;
+}
+
+
+void sendPacket()
 {
 	do 
 	{
@@ -62,11 +121,13 @@ void preamblePacketWithSkille()
 					{
 						state = 1;
 						isRunAgain = true;
+						counter = 0;
+            bitMask = 0x80;
 					}
 					break;
 
 			case 1: //address
-					whichBit = (bitMask & address == 0 ? 0 : 1);
+					whichBit = ((bitMask & address) == 0 ? 0 : 1);
 
 					if (counter == 8)
 					{
@@ -76,10 +137,13 @@ void preamblePacketWithSkille()
 					{
 						state = 2;
 						isRunAgain = true;
+						counter = 0;
+            bitMask = 0x80;
 					}
+         break;
 
 			case 2: //order
-					whichBit = (bitMask & order == 0 ? 0 : 1);
+					whichBit = ((bitMask & order) == 0 ? 0 : 1);
 
 					if (counter == 8)
 					{
@@ -89,11 +153,13 @@ void preamblePacketWithSkille()
 					{
 						state = 3;
 						isRunAgain = true;
+						counter = 0;
+            bitMask = 0x80;
 					}
 					break;
 
 			case 3: //checksum
-					whichBit = (bitMask & checksum == 0 ? 0 : 1);
+					whichBit = ((bitMask & checksum) == 0 ? 0 : 1);
 
 					if (counter == 8)
 					{
@@ -103,6 +169,13 @@ void preamblePacketWithSkille()
 					{
 						state = 0;
 						isRunAgain = true;
+						counter = 0;
+						if (packetType == 1)
+						{
+							accessoryStuff();
+						}
+
+           bitMask = 0x80;
 					}
 					break;
 		}
@@ -115,53 +188,55 @@ void preamblePacketWithSkille()
 	{
 		if (prevHighOrLow == 0 && highOrLow == 0)
 		{
-			digitalWrite(OUTPTN, HIGH);
+			digitalWrite(OUTPIN, HIGH);
 			prevHighOrLow = 0;
 			highOrLow = 1;
 			}
 
 	 		else if (prevHighOrLow == 0 && highOrLow == 1)
 			{
-				digitalWrite(OUTPTN, HIGH);
+				digitalWrite(OUTPIN, HIGH);
 				prevHighOrLow = 1;
 				highOrLow = 1;   
 			}
 
 			else if (prevHighOrLow == 1 && highOrLow == 1)
 			{
-				digitalWrite(OUTPTN, LOW);
+				digitalWrite(OUTPIN, LOW);
 				prevHighOrLow = 1;
 				highOrLow = 0;
 			}
 
 			else if (prevHighOrLow == 1 && highOrLow == 0)
 			{
-				digitalWrite(OUTPTN, LOW);
+				digitalWrite(OUTPIN, LOW);
 				prevHighOrLow = 0;
 				highOrLow = 0;
 				counter++;
+        bitMask >>= 1;
 			}
 	}
 	else if (whichBit == 1)
 	{
 		if (highOrLow == 0)
-			{
-				digitalWrite(OUTPTN, HIGH);
-				highOrLow = 1;
-			}
-			else if (highOrLow == 1)
-			{
-				digitalWrite(OUTPTN, LOW);
-				highOrLow = 0;
-				counter++;
-			}
+		{
+			digitalWrite(OUTPIN, HIGH);
+			highOrLow = 1;
+		}
+		else if (highOrLow == 1)
+		{
+			digitalWrite(OUTPIN, LOW);
+			highOrLow = 0;
+			counter++;
+      bitMask >>= 1;
+		}
 	}
 }
 
 
 ISR(TIMER2_COMPA_vect)
 {
-	
+	sendPacket();
 }
 
 void timer2_setup() 
